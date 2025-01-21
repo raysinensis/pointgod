50ecf07de24fd934cf9b51564697c28752ba8c65
raysinensis <raysinensis@gmail.com>
2025-01-20 22:29:04 -0500

    Initial Commit

---
+++ pointgod/pointgodApp.swift
@@ -0,0 +1,17 @@
+//
+//  pointgodApp.swift
+//  pointgod
+//
+//  Created by Rui Fu on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct pointgodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pointgodTests/pointgodTests.swift
@@ -0,0 +1,16 @@
+//
+//  pointgodTests.swift
+//  pointgodTests
+//
+//  Created by Rui Fu on 1/20/25.
+//
+
+import Testing
+
+struct pointgodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
